b9f6ced28b08f9d86743dd5d90cb53eabdfd13f7
zhuoyang <75078166+Azora04@users.noreply.github.com>
2026-08-21 15:38:07 +0800

    Level-0: Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Gary User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Gary.java
@@ -0,0 +1,18 @@
+public class Gary {
+    public static void main(String[] args) {
+        String banner = "██████╗   █████╗ ██████╗ ██╗   ██╗\n"
+                + "██╔════╝ ██╔══██╗██╔══██╗╚██╗ ██╔╝\n"
+                + "██║  ███╗███████║██████╔╝ ╚████╔╝ \n"
+                + "██║   ██║██╔══██║██╔══██╗  ╚██╔╝  \n"
+                + "╚██████╔╝██║  ██║██║  ██║   ██║   \n"
+                + " ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \n";
+        String line = "____________________________________________________________\n";
+        System.out.println(line + "\n" +
+                banner + "\n" +
+                "Hello! I'm Gary.\n" +
+                "What can I do for you?\n" +
+                line +
+                "Bye. Hope to see you again soon!\n" +
+                line);
+    }
+}
