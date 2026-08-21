b8e936457842bb1c71aa5825e88ab5a0e77264ac
zhuoyang <75078166+Azora04@users.noreply.github.com>
2026-08-21 15:57:32 +0800

    Level-3: Mark as Done

--- src/main/java/Gary.java
+++ src/main/java/Gary.java
@@ -16,7 +16,7 @@ public class Gary {
                 line + "\n");
 
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -29,11 +29,25 @@ public class Gary {
             }
 
             if (command.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
+                            + tasks[i].getDescription());
                 }
+            } else if (command.startsWith("mark ")) {
+                int taskNumber = Integer.parseInt(command.substring(5));
+                Task task = tasks[taskNumber - 1];
+                task.markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
+            } else if (command.startsWith("unmark ")) {
+                int taskNumber = Integer.parseInt(command.substring(7));
+                Task task = tasks[taskNumber - 1];
+                task.markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + task.getStatusIcon() + "] " + task.getDescription());
             } else {
-                tasks[taskCount] = command;
+                tasks[taskCount] = new Task(command);
                 taskCount++;
                 System.out.println("added: " + command);
             }
